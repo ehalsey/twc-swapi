d00f8c65db1be264e623143c22aa678ee0eb8b35
ehalsey <ehalsey@a-cto.com>
2020-05-21 19:29:47 -0700

    working

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-//check out https://www.pluralsight.com/guides/how-to-test-react-components-in-typescript
-test('renders person list', async () => {
-  const wrapper = render(<App />);
-  //expect(getByText('Luke Skywalker')).toBeInTheDocument();
-});
